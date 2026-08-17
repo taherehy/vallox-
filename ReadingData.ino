c38aa0e216b4011bad9d7df980ef1edf01b99d68
Pikkarav <156898416+Pikkarav@users.noreply.github.com>
2026-08-17 13:44:58 +0300

    ReadingDataVol2.ino

--- ReadingData.ino
+++ ReadingData.ino
@@ -1,29 +1,34 @@
 #include <HardwareSerial.h>
-// Reading data from vallox simulator
-HardwareSerial RS485(2);
 
-#define RX_PIN 4
-#define TX_PIN 5
+HardwareSerial RS485(2);
 
 void setup() {
   Serial.begin(115200);
-  RS485.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  RS485.begin(9600, SERIAL_8N1, 4, 5);
 
-  Serial.println("Reading full packet...");
+  Serial.println("Vallox sniffer");
 }
 
 void loop() {
-  if (RS485.available()) {
-    Serial.print("Packet: ");
 
-    while (RS485.available()) {
-      int b = RS485.read();
-      Serial.print(b, HEX);
-      Serial.print(" ");
+  static int bytePos = 0;
+
+  while (RS485.available()) {
+
+    uint8_t b = RS485.read();
+
+    // Uusi kehys alkaa aina DOMAIN-arvosta 0x01
+    if (b == 0x01 && bytePos != 0) {
+      Serial.println();
+      bytePos = 0;
     }
 
-    Serial.println();
-  }
+    if (b < 0x10)
+      Serial.print("0");
 
-  delay(200);
+    Serial.print(b, HEX);
+    Serial.print(" ");
+
+    bytePos++;
+  }
 }
