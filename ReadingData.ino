b3eb2934d3ec5fceed4a5bf67993d5ddd9d440da
Pikkarav <156898416+Pikkarav@users.noreply.github.com>
2026-08-24 09:29:32 +0300

    Update ReadingData.ino

--- ReadingData.ino
+++ ReadingData.ino
@@ -1,34 +1,31 @@
 #include <HardwareSerial.h>
+#include "ValloxDecoder.h"
 
 HardwareSerial RS485(2);
+ValloxDecoder decoder;
 
 void setup() {
-  Serial.begin(115200);
-  RS485.begin(9600, SERIAL_8N1, 4, 5);
 
-  Serial.println("Vallox sniffer");
+    Serial.begin(115200);
+
+    RS485.begin(
+        9600,
+        SERIAL_8N1,
+        4,
+        5
+    );
 }
 
 void loop() {
 
-  static int bytePos = 0;
+    if (RS485.available() >= 6) {
 
-  while (RS485.available()) {
+        uint8_t frame[6];
 
-    uint8_t b = RS485.read();
+        for(int i = 0; i < 6; i++) {
+            frame[i] = RS485.read();
+        }
 
-    // Uusi kehys alkaa aina DOMAIN-arvosta 0x01
-    if (b == 0x01 && bytePos != 0) {
-      Serial.println();
-      bytePos = 0;
+        decoder.decode(frame);
     }
-
-    if (b < 0x10)
-      Serial.print("0");
-
-    Serial.print(b, HEX);
-    Serial.print(" ");
-
-    bytePos++;
-  }
 }
